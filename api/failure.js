0918113396016f0c105b454731eb9960bcfa42fb
Arnout Kazemier <info@3rd-Eden.com>
2021-08-21 18:44:26 +0200

    [fix] Remove private information from error messages

--- api/failure.js
+++ api/failure.js
@@ -10,11 +10,17 @@
  * @public
  */
 module.exports = async function failure({ bundle, error }) {
+  //
+  // Clean up the error message by removing any possible path to prevent leaking
+  // somewhat private information to our users.
+  //
+  const message = error.replace(/\/[^\s'"]+/g, '(private)');
+
   const payload = `
     if (typeof console !== 'undefined' && console.error && console.group) {
       [
         ['group', '500: An error occured while loading bundle: '+ ${JSON.stringify(bundle)}],
-        ['error', 'Error message: '+ ${JSON.stringify(error)}],
+        ['error', 'Error message: '+ ${JSON.stringify(message)}],
         ['error', 'This is most likely caused by an error in your bundle handler'],
         ['error', 'Additional info: https://github.com/3rd-Eden/payper/tree/main/api#failure'],
         ['groupEnd']
